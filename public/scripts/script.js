255e33e6ec742d3b62b215cf913915044520c1e1
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-28 16:49:13 -0500

    EOD push

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,16 +1,45 @@
 console.log('script sourced');
 
+var lock = new Auth0Lock( 'tADNo2G8xWUWbf4EraRWTRKP8mOBv9xB', 'natebiessener.auth0.com');
+// log out url, from Auth0
+var logOutUrl = 'https://natebiessener.auth0.com/v2/logout';
+
 var myApp = angular.module('myApp', []);
 
-myApp.controller('displayController', ['$scope', 'ShelfFactory', function($scope, ShelfFactory){
+myApp.controller('displayController', ['$scope', '$http', 'ShelfFactory', function($scope, $http, ShelfFactory){
   $scope.loggedIn = false;
 
   $scope.logIn = function(){
-    $scope.loggedIn = true;
+    lock.show( function( err, profile, token ) {
+      if (err) {
+        console.error( "auth error: ", err);
+      } // end error
+      else {
+        // save token to localStorage
+        localStorage.setItem( 'userToken', token );
+        // save user profile to localStorage
+        localStorage.setItem( 'userProfile', JSON.stringify( profile ) );
+                              // reload page because dirtyhaxorz
+                              // location.reload();
+
+      } // end no error
+    }); //end lock.show
+    $scope.loggedIn = true; //<--FIX THIS, RUNS IF(ERR), DOES NOT WORK AS INTENDED INSIDE ELSE BLOCK
   };
 
   $scope.logOut = function(){
-    $scope.loggedIn = false;
+    $http({
+      method:'GET',
+      url: logOutUrl,
+    }).then( function( data ){
+      // if logged out OK
+      if( data.data == 'OK' ){
+        // empty localStorage
+        emptyLocalStorage();
+        $scope.loggedIn = false;
+      }
+    })
+
   };
   console.log('ng');
 
@@ -34,8 +63,7 @@ myApp.controller('displayController', ['$scope', 'ShelfFactory', function($scope
 }]);
 
 
-myApp.controller('privelegedController', ['$scope', 'ShelfFactory', function($scope, ShelfFactory){
-
-
-
-}]);
+var emptyLocalStorage = function(){
+  localStorage.removeItem( 'userProfile' );
+  localStorage.removeItem( 'userToken' );
+}; // end emptyLocalStorage
