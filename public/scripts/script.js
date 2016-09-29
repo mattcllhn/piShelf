047b804ff032ebc6aa395d3626b0748858bc707b
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-28 21:01:01 -0500

    add page refresh hack, finish objectToSend assembly for putThingOnShelf

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,37 +1,51 @@
 console.log('script sourced');
 
-var lock = new Auth0Lock( 'tADNo2G8xWUWbf4EraRWTRKP8mOBv9xB', 'natebiessener.auth0.com');
+var lock = new Auth0Lock('tADNo2G8xWUWbf4EraRWTRKP8mOBv9xB', 'natebiessener.auth0.com');
 // log out url, from Auth0
 var logOutUrl = 'https://natebiessener.auth0.com/v2/logout';
 
 var myApp = angular.module('myApp', []);
 
 myApp.controller('displayController', ['$scope', '$http', 'ShelfFactory', function($scope, $http, ShelfFactory){
-  $scope.loggedIn = false;
+  $scope.onLoad = function(){
+    console.log('in onLoad');
+    // check if a user's info is saved in localStorage
+    if(JSON.parse(localStorage.getItem('userProfile'))){
+      // if so, save userProfile as $scope.userProfile
+      $scope.userProfile = JSON.parse( localStorage.getItem('userProfile'));
+      console.log('loggedIn:', $scope.userProfile);
+      $scope.loggedIn = true;
+    }
+    else{
+      // if not, make sure we are logged out and empty
+      emptyLocalStorage();
+      $scope.loggedIn = false;
+    }
+  };
+
+  $scope.onLoad();
 
   $scope.logIn = function(){
-    lock.show( function( err, profile, token ) {
+    lock.show(function(err, profile, token) {
       if (err) {
-        console.error( "auth error: ", err);
+        console.error("auth error:", err);
       } // end error
       else {
         // save token to localStorage
-        localStorage.setItem( 'userToken', token );
+        localStorage.setItem('userToken', token);
         // save user profile to localStorage
-        localStorage.setItem( 'userProfile', JSON.stringify( profile ) );
-                              // reload page because dirtyhaxorz
-                              // location.reload();
-
+        localStorage.setItem('userProfile', JSON.stringify(profile));
+        // reload page because dirtyhaxorz
+        location.reload();
       } // end no error
-    }); //end lock.show
-    $scope.loggedIn = true; //<--FIX THIS, RUNS IF(ERR), DOES NOT WORK AS INTENDED INSIDE ELSE BLOCK
+    });
   };
 
   $scope.logOut = function(){
     $http({
       method:'GET',
       url: logOutUrl,
-    }).then( function( data ){
+    }).then(function(data){
       // if logged out OK
       if( data.data == 'OK' ){
         // empty localStorage
@@ -39,31 +53,32 @@ myApp.controller('displayController', ['$scope', '$http', 'ShelfFactory', functi
         $scope.loggedIn = false;
       }
     })
-
   };
-  console.log('ng');
 
   $scope.shelf = [];
   //fill shelf on load
-  ShelfFactory.fillShelf().then(
-    function(){
-      console.log(ShelfFactory.shelf());
-      $scope.shelf = ShelfFactory.shelf();
+  var showShelf = function(){
+    ShelfFactory.fillShelf().then(
+      function(){
+        console.log(ShelfFactory.shelf());
+        $scope.shelf = ShelfFactory.shelf();
     });
+  };
+  showShelf();
 
-    var putThingOnShelf = function(){
-      var thing = {
-        description: $scope.descriptionIn,
-        owner: undefined, //!!---TAKE FROM AUTH0
-        imageUrl: $scope.imageIn
-      };
-      return ShelfFactory.addToShelf(thing);
+  $scope.putThingOnShelf = function(){
+    var thing = {
+      description: $scope.descriptionIn,
+      owner: $scope.userProfile.name,
+      imageUrl: $scope.imageIn
     };
+    ShelfFactory.addToShelf(thing).then(showShelf);
+  };
 
 }]);
 
 
 var emptyLocalStorage = function(){
-  localStorage.removeItem( 'userProfile' );
-  localStorage.removeItem( 'userToken' );
+  localStorage.removeItem('userProfile');
+  localStorage.removeItem('userToken');
 }; // end emptyLocalStorage
